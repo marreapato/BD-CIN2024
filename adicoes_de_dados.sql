c94de1c66bb02465072a3ee7e888b679f36012f1
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-18 20:29:46 -0300

    Update adicoes_de_dados.sql

--- adicoes_de_dados.sql
+++ adicoes_de_dados.sql
@@ -3,5 +3,31 @@ VALUES (1,'Hotel ABC',tp_endereco('Brasil','30289-020','São Paulo','São Paulo'
 
 SELECT * FROM HOTEL;
 
-SELECT H.PKID_HOTEL, H.NOME, H.ENDERECO_HOTEL.CEP, H.ENDERECO_HOTEL.PROVINCIA FROM HOTEL H WHERE PKID_HOTEL=1;
+SELECT H.PKID_HOTEL, H.NOME, H.ENDERECO_HOTEL.CEP, H.ENDERECO_HOTEL.ESTADO FROM HOTEL H;
 
+--voo
+
+INSERT INTO VOO (pk_numero_voo,origem,destino,hora_embarque,hora_desembarque)
+VALUES(1,
+'GRU',
+'BSB',
+TO_TIMESTAMP('08:20', 'HH24:,MI'),
+TO_TIMESTAMP('11:45','HH24:MI'));
+
+SELECT * FROM VOO;
+SELECT ORIGEM,DESTINO,TO_CHAR(hora_embarque,'HH24:MI'),TO_CHAR(hora_desembarque,'HH24:MI') FROM VOO;
+--SELECT * FROM VOO;--consertando problema
+--alter session set ddl_lock_timeout = 20;
+--DROP TABLE VOO;
+--commit;
+--rollback;
+
+
+INSERT INTO PASSAGEM (pk_numero_passagem,valor_passagem,data_ida,data_chegada)
+VALUES(1,
+504.23,
+TO_DATE('08:20', 'HH24:,MI'),
+TO_DATE('11:45','HH24:MI'));
+
+SELECT * FROM VOO;
+SELECT TO_CHAR(hora_embarque,'HH24:MI'),TO_CHAR(hora_desembarque,'HH24:MI') FROM VOO;
