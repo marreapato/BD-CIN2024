3a8b8d568e0ac2f5924af4254d23597eb38119d6
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-23 19:35:28 -0300

    Update voos.sql

--- PROJETO_SBDOR/inserts/voos.sql
+++ PROJETO_SBDOR/inserts/voos.sql
@@ -1,172 +1,172 @@
--- Flight 1
+--  1
 INSERT INTO VOO_TABLE VALUES (
     1,               -- pk_localizador_voo
-    'GRU',           -- origem (São Paulo - Guarulhos International Airport)
-    'GIG',           -- destino (Rio de Janeiro - Galeão International Airport)
-    TO_TIMESTAMP('12:00:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('13:30:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'GRU',           -- origem 
+    'GIG',           -- destino 
+    TO_TIMESTAMP('12:00:00', 'HH24:MI:SS'),  -- hora_embarque 
+    TO_TIMESTAMP('13:30:00', 'HH24:MI:SS'),  -- hora_desembarque 
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678901'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 1)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678901'),  -- passageiros 
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 1)   -- passagem
         )
     )
 );
 
--- Flight 2
+--  2
 INSERT INTO VOO_TABLE VALUES (
     2,               -- pk_localizador_voo
-    'CGH',           -- origem (São Paulo - Congonhas Airport)
-    'BSB',           -- destino (Brasília International Airport)
-    TO_TIMESTAMP('10:30:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('12:15:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'CGH',           -- origem 
+    'BSB',           -- destino 
+    TO_TIMESTAMP('10:30:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('12:15:00', 'HH24:MI:SS'),  -- hora_desembarque 
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '23456789012'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 2)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '23456789012'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 2)   -- passagem
         )
     )
 );
 
--- Flight 3
+--  3
 INSERT INTO VOO_TABLE VALUES (
     3,               -- pk_localizador_voo
-    'BSB',           -- origem (Brasília International Airport)
-    'CNF',           -- destino (Belo Horizonte - Confins International Airport)
-    TO_TIMESTAMP('14:00:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('15:45:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'BSB',           -- origem
+    'CNF',           -- destino
+    TO_TIMESTAMP('14:00:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('15:45:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '34567890123'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 3)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '34567890123'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 3)   -- passagem
         )
     )
 );
 
--- Flight 4
+--  4
 INSERT INTO VOO_TABLE VALUES (
     4,               -- pk_localizador_voo
-    'GIG',           -- origem (Rio de Janeiro - Galeão International Airport)
-    'SSA',           -- destino (Salvador International Airport)
-    TO_TIMESTAMP('09:45:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'GIG',           -- origem
+    'SSA',           -- destino
+    TO_TIMESTAMP('09:45:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '45678901234'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 4)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '45678901234'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 4)   -- passagem
         )
     )
 );
 
--- Flight 5
+--  5
 INSERT INTO VOO_TABLE VALUES (
     5,               -- pk_localizador_voo
-    'CNF',           -- origem (Belo Horizonte - Confins International Airport)
-    'REC',           -- destino (Recife International Airport)
-    TO_TIMESTAMP('13:20:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('16:10:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'CNF',           -- origem
+    'REC',           -- destino
+    TO_TIMESTAMP('13:20:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('16:10:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '56789012345'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 5)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '56789012345'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 5)   -- passagem
         ),tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '67890123456'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 6)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '67890123456'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 6)   -- passagem
         )
     )
 );
--- Inserting next 5 rows of sample data into VOO_TABLE
 
--- Flight 6
+
+
 INSERT INTO VOO_TABLE VALUES (
     6,               -- pk_localizador_voo
-    'SSA',           -- origem (Salvador International Airport)
-    'REC',           -- destino (Recife International Airport)
-    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('14:00:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'SSA',           -- origem
+    'REC',           -- destino
+    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('14:00:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
       
     )
 );
 
--- Flight 7
+--  7
 INSERT INTO VOO_TABLE VALUES (
     7,               -- pk_localizador_voo
-    'REC',           -- origem (Recife International Airport)
-    'FOR',           -- destino (Fortaleza International Airport)
-    TO_TIMESTAMP('08:45:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('11:15:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'REC',           -- origem
+    'FOR',           -- destino
+    TO_TIMESTAMP('08:45:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('11:15:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '78901234567'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 7)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '78901234567'),  -- passageiros 
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 7)   -- passagem 
         )
     )
 );
 
--- Flight 8
+--  8
 INSERT INTO VOO_TABLE VALUES (
     8,               -- pk_localizador_voo
-    'CNF',           -- origem (Belo Horizonte - Confins International Airport)
-    'POA',           -- destino (Porto Alegre International Airport)
-    TO_TIMESTAMP('14:20:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('16:30:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'CNF',           -- origem
+    'POA',           -- destino
+    TO_TIMESTAMP('14:20:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('16:30:00', 'HH24:MI:SS'),  -- hora_desembarque 
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '89012345678'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 8)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '89012345678'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 8)   -- passagem
         )
     )
 );
 
--- Flight 9
+--  9
 INSERT INTO VOO_TABLE VALUES (
     9,               -- pk_localizador_voo
-    'POA',           -- origem (Porto Alegre International Airport)
-    'FLN',           -- destino (Florianópolis International Airport)
-    TO_TIMESTAMP('10:00:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'POA',           -- origem
+    'FLN',           -- destino
+    TO_TIMESTAMP('10:00:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('11:30:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '90123456789'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 9)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '90123456789'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 9)   -- passagem
         )
     )
 );
 
--- Flight 10
+--  10
 INSERT INTO VOO_TABLE VALUES (
     10,              -- pk_localizador_voo
-    'BSB',           -- origem (Brasília International Airport)
-    'POA',           -- destino (Porto Alegre International Airport)
-    TO_TIMESTAMP('12:45:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('15:00:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'BSB',           -- origem
+    'POA',           -- destino
+    TO_TIMESTAMP('12:45:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('15:00:00', 'HH24:MI:SS'),  -- hora_desembarque 
+    tp_nt_ref_relac(  -- compras 
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '01234567890'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 10)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '01234567890'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 10)   -- passagem
         )
     )
 );
 
 
--- Inserting one more row of sample data into VOO_TABLE
 
--- Flight 11
+
+--  11
 INSERT INTO VOO_TABLE VALUES (
     11,              -- pk_localizador_voo
-    'SSA',           -- origem (Salvador International Airport)
-    'CNF',           -- destino (Belo Horizonte - Confins International Airport)
-    TO_TIMESTAMP('16:30:00', 'HH24:MI:SS'),  -- hora_embarque (boarding time)
-    TO_TIMESTAMP('18:00:00', 'HH24:MI:SS'),  -- hora_desembarque (arrival time)
-    tp_nt_ref_relac(  -- compras (list of passenger-ticket references)
+    'SSA',           -- origem
+    'CNF',           -- destino
+    TO_TIMESTAMP('16:30:00', 'HH24:MI:SS'),  -- hora_embarque
+    TO_TIMESTAMP('18:00:00', 'HH24:MI:SS'),  -- hora_desembarque
+    tp_nt_ref_relac(  -- compras
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678922'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 11)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678922'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 11)   -- passagem
         ),
         tp_ref_relac(
-            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '23456289012'),  -- passageiros (passenger reference)
-            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 12)   -- passagem (ticket reference)
+            (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '23456289012'),  -- passageiros
+            (SELECT REF(pa) FROM PASSAGEM pa WHERE pa.pk_localizador_passagem = 12)   -- passagem
         )
     )
 );
