05e9877851b5f248d03a4a030174ce2db96d7da7
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-23 19:28:52 -0300

    Update procedures.sql

--- PROJETO_SBDOR/procedures.sql
+++ PROJETO_SBDOR/procedures.sql
@@ -7,14 +7,13 @@ CREATE OR REPLACE PROCEDURE MoveRecordsToMaiorIdade AS
 
     v_idade NUMBER;
 BEGIN
-    -- Loop through the cursor to process each record in MENOR_IDADE_TB
+
     FOR menor_rec IN menor_cursor LOOP
-        -- Calculate age using the calcular_idade_trigger function
+
         v_idade := calcular_idade_trigger(menor_rec.DATA_NASCIMENTO);
 
-        -- Check if the age is 18 years or older
         IF v_idade >= 18 THEN
-            -- Insert the record into MAIOR_IDADE_TB
+
             INSERT INTO MAIOR_IDADE_TB (
                 PK_CPF,
                 NOME,
@@ -33,13 +32,13 @@ BEGIN
                 menor_rec.EMAIL
             );
 
-            -- Delete the record from MENOR_IDADE_TB
+
             DELETE FROM MENOR_IDADE_TB
-            WHERE PK_CPF = menor_rec.PK_CPF; -- Assuming PK_CPF is the primary key
+            WHERE PK_CPF = menor_rec.PK_CPF; 
         END IF;
     END LOOP;
 
-    -- Commit the transaction to apply changes
+
     COMMIT;
 END MoveRecordsToMaiorIdade;
 /
@@ -51,9 +50,9 @@ BEGIN
     SET ACOMPANHAMENTO_ESPECIAL = 'nao'
     WHERE calcular_idade_trigger(DATA_NASCIMENTO) >= 16;
     
-    -- Commit the transaction to apply changes
+
     COMMIT;
     
-    -- Display a success message
+
     DBMS_OUTPUT.PUT_LINE('Acompanhamento Viagem updated successfully.');
 END UpdateAcompanhamentoViagem;
